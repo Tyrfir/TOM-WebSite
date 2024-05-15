3ff17d2df5bf1c5eec970ce69d6f7008faf5c1af
Tyr <weslleyrock06@gmail.com>
2024-05-15 01:48:57 -0300

    A página que seria de login foi usada como págica de teste javaScript

--- script/Log-Sig.js
+++ script/Log-Sig.js
@@ -1,4 +1,24 @@
 function myFunction()
 {
-    alert("o script ta funcionando kralho!");
+    var nome = window.prompt("Digite seu nome!");
+    alert("Ola " + nome)
 }
+
+function calcFerias(){
+    var salario = Number(window.prompt("Qual seu salário?"))
+    var QuantMes = window.confirm("Sua férias é de apenas 1 mês?")
+
+    // calc férias salario + 1/3 salario
+    var ferias = salario/3 + salario
+    //var ferias2 = salario + ferias1
+
+    if(QuantMes == true){
+        alert("Voce receberá: " + ferias)
+    }
+    else{
+        var diasFerias = Number.parseInt(window.prompt("Quantos dias você sairá de férias?"))
+        var totalDiasFerias = salario/30 * diasFerias + salario/3
+
+        alert("Então você receberá: " + totalDiasFerias.toString())
+    }
+} 

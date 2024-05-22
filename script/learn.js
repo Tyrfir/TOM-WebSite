5c8eaaceea57ab1b34e60df0e6d4a5adde4f47d9
Tyrfir <weslleyrock06@gmail.com>
2024-05-21 22:27:56 -0300

    Adicionado aula 2 de JavaScript

--- script/learn.js
+++ script/learn.js
@@ -1,1 +1,14 @@
-console.log('ola');
+const nome = 'Weslley';
+const sobreNome = 'de Jesus';
+const idade = 25;
+const peso = 105;
+const altura = 1.85;
+let imc;
+let anoNascimento;
+
+imc = peso / (altura * altura)
+anoNascimento = 2024 - idade
+
+console.log(`${nome} ${sobreNome} nasceu em ${anoNascimento} tem ${idade}
+anos, pesa ${peso}kg e tem ${altura} de altura, seu IMC é ${imc}
+`)

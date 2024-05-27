ac40d99ee55a3de635912e0e67dcfd2df7d0c324
Tyrfir <weslleyrock06@gmail.com>
2024-05-26 21:34:04 -0300

    Adicionado exercício da aula 3

--- script/learn.js
+++ script/learn.js
@@ -16,10 +16,27 @@ anos, pesa ${peso}kg e tem ${altura} de altura, seu IMC é ${imc}
 
 // EX:. 2
 
-let varA = 'A';
+/*let varA = 'A';
 let varB = 'B';
 let varC = 'C';
 
 [varA, varB, varC] = [varB, varC, varA]
 
-console.log(varA, varB, varC)
+console.log(varA, varB, varC) */
+
+//Ex: 3
+
+let alfabeto = "abcdefghijklmnopqrstuvxzwy"
+let trocando = 'Dias de luta e dias de glória'
+
+console.log(alfabeto[24]) //imprime a letra respectiva do indice da variável
+
+console.log(alfabeto.indexOf('w')) // mostra o local da letra W
+
+console.log(alfabeto.replace(/w/g, ' 24 ')) // troca uma letra por algum caracter
+
+console.log(trocando.replace(/ /g, '-')) // troca uma letra por algum caracter
+
+console.log(trocando.length) //Mostra qantos caracters tem a string
+
+

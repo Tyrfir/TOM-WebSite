8ca8dfba233fd5611340bf7e51f4a4281b06a0e8
Tyrfir <weslleyrock06@gmail.com>
2024-05-26 21:16:45 -0300

    Adicionado exercício da aula 2

--- script/learn.js
+++ script/learn.js
@@ -1,4 +1,5 @@
-const nome = 'Weslley';
+// EX:. 1
+/* const nome = 'Weslley';
 const sobreNome = 'de Jesus';
 const idade = 25;
 const peso = 105;
@@ -11,4 +12,14 @@ anoNascimento = 2024 - idade
 
 console.log(`${nome} ${sobreNome} nasceu em ${anoNascimento} tem ${idade}
 anos, pesa ${peso}kg e tem ${altura} de altura, seu IMC é ${imc}
-`)
+`) */
+
+// EX:. 2
+
+let varA = 'A';
+let varB = 'B';
+let varC = 'C';
+
+[varA, varB, varC] = [varB, varC, varA]
+
+console.log(varA, varB, varC)
